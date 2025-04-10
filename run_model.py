afca0419d1664dc97e4aa4c4aa42d17171a03349
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-04-10 13:53:39 -0400

    black formatting

--- run_model.py
+++ run_model.py
@@ -14,39 +14,38 @@ breeder_thickness = 40
 # Density: 2.3 g/cm3
 # Reference: PNNL Report 15870 (Rev. 1)
 # Describes: facility foundation, floors, walls
-Concrete = openmc.Material(name='Concrete') 
-Concrete.set_density('g/cm3', 2.3) 
-Concrete.add_nuclide('H1', 0.168759, 'ao') 
-Concrete.add_element('C', 0.001416, 'ao') 
-Concrete.add_nuclide('O16', 0.562524, 'ao') 
-Concrete.add_nuclide('Na23', 0.011838, 'ao') 
-Concrete.add_element('Mg', 0.0014, 'ao') 
-Concrete.add_nuclide('Al27', 0.021354, 'ao') 
-Concrete.add_element('Si', 0.204115, 'ao') 
-Concrete.add_element('K', 0.005656, 'ao') 
-Concrete.add_element('Ca', 0.018674, 'ao') 
-Concrete.add_element('Fe', 0.004264, 'ao') 
+Concrete = openmc.Material(name="Concrete")
+Concrete.set_density("g/cm3", 2.3)
+Concrete.add_nuclide("H1", 0.168759, "ao")
+Concrete.add_element("C", 0.001416, "ao")
+Concrete.add_nuclide("O16", 0.562524, "ao")
+Concrete.add_nuclide("Na23", 0.011838, "ao")
+Concrete.add_element("Mg", 0.0014, "ao")
+Concrete.add_nuclide("Al27", 0.021354, "ao")
+Concrete.add_element("Si", 0.204115, "ao")
+Concrete.add_element("K", 0.005656, "ao")
+Concrete.add_element("Ca", 0.018674, "ao")
+Concrete.add_element("Fe", 0.004264, "ao")
 
 air = openmc.Material(name="Air")
-air.add_element("C", 0.00012399 , 'wo')
-air.add_element('N', 0.75527, 'wo')
-air.add_element('O', 0.23178, 'wo')
-air.add_element('Ar', 0.012827, 'wo')
-air.set_density('g/cm3', 0.0012)
+air.add_element("C", 0.00012399, "wo")
+air.add_element("N", 0.75527, "wo")
+air.add_element("O", 0.23178, "wo")
+air.add_element("Ar", 0.012827, "wo")
+air.set_density("g/cm3", 0.0012)
 
 floor_rpp = openmc.model.RectangularParallelepiped(-300, 300, -300, 300, -150, -100)
-floor_rpp.xmin.boundary_type='vacuum'
-floor_rpp.xmax.boundary_type='vacuum'
-floor_rpp.ymin.boundary_type='vacuum'
-floor_rpp.ymax.boundary_type='vacuum'
-floor_rpp.zmin.boundary_type='vacuum'
-boundary_top_plane = openmc.ZPlane(200, boundary_type='vacuum')
+floor_rpp.xmin.boundary_type = "vacuum"
+floor_rpp.xmax.boundary_type = "vacuum"
+floor_rpp.ymin.boundary_type = "vacuum"
+floor_rpp.ymax.boundary_type = "vacuum"
+floor_rpp.zmin.boundary_type = "vacuum"
+boundary_top_plane = openmc.ZPlane(200, boundary_type="vacuum")
 
-floor_cell = openmc.Cell(region=-floor_rpp, fill=Concrete,
-                         name='floor')
+floor_cell = openmc.Cell(region=-floor_rpp, fill=Concrete, name="floor")
 
 settings = openmc.Settings()
-settings.run_mode = 'fixed source'
+settings.run_mode = "fixed source"
 settings.batches = 100
 settings.inactive = 0
 settings.particles = int(1e5)
@@ -54,45 +53,49 @@ settings.particles = int(1e5)
 # settings.photon_transport = True
 settings.photon_transport = False
 
-directory = f'BrHt={breeder_height:.0f}cm_BrTh={breeder_thickness:.0f}_RefTh={reflector_thickness:.0f}'
+directory = f"BrHt={breeder_height:.0f}cm_BrTh={breeder_thickness:.0f}_RefTh={reflector_thickness:.0f}"
 if not os.path.isdir(directory):
-      os.mkdir(directory)
+    os.mkdir(directory)
 os.chdir(directory)
 
-libra_reg, libra_system_cell, materials, src, salt_cell, salt_material, salt_vol = \
-        single_wall_model.build_libra_xl(salt_height=breeder_height,
-                                salt_thickness=breeder_thickness,
-                                reflector_thickness=reflector_thickness)
+libra_reg, libra_system_cell, materials, src, salt_cell, salt_material, salt_vol = (
+    single_wall_model.build_libra_xl(
+        salt_height=breeder_height,
+        salt_thickness=breeder_thickness,
+        reflector_thickness=reflector_thickness,
+    )
+)
 materials.append(Concrete)
 materials.append(air)
 
-outside_air_reg = +floor_rpp.xmin & -floor_rpp.xmax \
-                & +floor_rpp.ymin & -floor_rpp.ymax \
-                & +floor_rpp.zmax & -boundary_top_plane \
-                & ~libra_reg
-outside_air_cell = openmc.Cell(region=outside_air_reg,
-                            fill=air,
-                            name='outside air')
+outside_air_reg = (
+    +floor_rpp.xmin
+    & -floor_rpp.xmax
+    & +floor_rpp.ymin
+    & -floor_rpp.ymax
+    & +floor_rpp.zmax
+    & -boundary_top_plane
+    & ~libra_reg
+)
+outside_air_cell = openmc.Cell(region=outside_air_reg, fill=air, name="outside air")
 
-universe = openmc.Universe(cells=[libra_system_cell, 
-                                floor_cell,
-                                outside_air_cell])
+universe = openmc.Universe(cells=[libra_system_cell, floor_cell, outside_air_cell])
 geometry = openmc.Geometry(universe)
 
-settings.source = src 
+settings.source = src
 
 
-t_tally = openmc.Tally(name='tritium tally')
+t_tally = openmc.Tally(name="tritium tally")
 salt_filter = openmc.MaterialFilter([salt_material])
 salt_cell_filter = openmc.CellFilter([salt_cell])
 t_tally.filters.append(salt_filter)
 t_tally.filters.append(salt_cell_filter)
-t_tally.scores = ['(n,Xt)']
+t_tally.scores = ["(n,Xt)"]
 
-t_li_tally = openmc.Tally(name='tritium lithium tally')
+t_li_tally = openmc.Tally(name="tritium lithium tally")
 t_li_tally.filters.append(salt_filter)
-t_li_tally.nuclides = ['Li6', 'Li7']
-t_li_tally.scores = ['(n,Xt)']
+t_li_tally.nuclides = ["Li6", "Li7"]
+t_li_tally.scores = ["(n,Xt)"]
 
 
 tallies = openmc.Tallies([t_tally, t_li_tally])
@@ -100,27 +103,30 @@ tallies = openmc.Tallies([t_tally, t_li_tally])
 plot1 = openmc.Plot.from_geometry(geometry)
 plot1.pixels = (1000, 1500)
 plot1.width = [200, 300]
-plot1.basis = 'xz'
-plot1.origin = [0,0,25]
-plot1.color_by = 'material'
+plot1.basis = "xz"
+plot1.origin = [0, 0, 25]
+plot1.color_by = "material"
 
 plots = openmc.Plots([plot1])
 
-model = openmc.Model(geometry=geometry,
-                    materials=materials,
-                    settings=settings,
-                    tallies=tallies,
-                    plots=plots)
+model = openmc.Model(
+    geometry=geometry,
+    materials=materials,
+    settings=settings,
+    tallies=tallies,
+    plots=plots,
+)
 model.export_to_model_xml()
 model.plot_geometry()
-if not os.path.exists('statepoint.100.h5'):
+if not os.path.exists("statepoint.100.h5"):
     model.run(threads=14)
 
-sp = openmc.StatePoint('statepoint.100.h5')
-t_tally = sp.get_tally(name='tritium tally')
-tbr = np.sum(t_tally.get_reshaped_data(value='mean').squeeze())
-tbr_err = np.sqrt(np.sum(np.square(t_tally.get_reshaped_data(value='std_dev').squeeze())))
-os.chdir('..')
+sp = openmc.StatePoint("statepoint.100.h5")
+t_tally = sp.get_tally(name="tritium tally")
+tbr = np.sum(t_tally.get_reshaped_data(value="mean").squeeze())
+tbr_err = np.sqrt(
+    np.sum(np.square(t_tally.get_reshaped_data(value="std_dev").squeeze()))
+)
+os.chdir("..")
 
-print('TBR = {:.4f} +/- {:.4f}'.format(tbr, tbr_err))
-
+print("TBR = {:.4f} +/- {:.4f}".format(tbr, tbr_err))
