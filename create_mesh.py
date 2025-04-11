bd7e81037b4b0688f115c6e1684d30eee51b82c8
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-04-11 15:15:33 -0400

    working transient model

--- create_mesh.py
+++ create_mesh.py
@@ -16,17 +16,38 @@ gmsh.model.geo.synchronize()
 
 # Add a physical group for the volume
 volume_tags = gmsh.model.getEntities(dim=3)  # Get all 3D entities (volumes)
+salt_volume_tag = 1
+
 if volume_tags:
     gmsh.model.addPhysicalGroup(
-        3, [v[1] for v in volume_tags], tag=1
+        3, [v[1] for v in volume_tags], tag=salt_volume_tag
     )  # Add all volumes to a physical group with tag 1
-    gmsh.model.setPhysicalName(3, 1, "Volume")  # Set a name for the physical group
+    gmsh.model.setPhysicalName(
+        3, salt_volume_tag, "Volume"
+    )  # Set a name for the physical group
+
+surface_tags = gmsh.model.getEntities(dim=2)  # Get all 2D entities (surfaces)
+print(len(surface_tags))
+
+tags = {
+    "top": [1, [0]],
+    "wall": [2, [1, 2, 4, 6, 7]],
+    "heater": [3, [3, 5]],
+}
+
+for name, (tag, surfaces) in tags.items():
+    gmsh.model.addPhysicalGroup(
+        2, [surface_tags[i][1] for i in surfaces], tag=tag, name=name
+    )
+
+gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 10)  # Maximum element size
 
 gmsh.model.mesh.generate(3)
-gmsh.model.mesh.setOrder(2)
+
+# gmsh.model.mesh.setOrder(2)  # uncomment to use quadratic elements
 
 gmsh.model.geo.synchronize()
-gmsh.fltk.run()
+# gmsh.fltk.run()  # uncomment to show the GUI
 
 
 # save mesh to .msh file
--- festim_model.py
+++ festim_model.py
@@ -8,13 +8,57 @@ reader = openmc2dolfinx.StructuredGridReader("out.vtk")
 from dolfinx.io import gmshio, XDMFFile
 from mpi4py import MPI
 
-# mesh = gmshio.read_from_msh("wedge.msh", comm=MPI.COMM_WORLD)
 
 from create_mesh import gmsh, mesh_comm, model_rank
 
-mesh, ct, _ = gmshio.model_to_mesh(gmsh.model, comm=mesh_comm, rank=model_rank)
+mesh, ct, ft = gmshio.model_to_mesh(gmsh.model, comm=mesh_comm, rank=model_rank)
+
+mesh.geometry.x[:,]
 
 
 with XDMFFile(MPI.COMM_WORLD, "mt.xdmf", "w") as xdmf:
     xdmf.write_mesh(mesh)
     xdmf.write_meshtags(ct, mesh.geometry)
+
+with XDMFFile(MPI.COMM_WORLD, "ft.xdmf", "w") as xdmf:
+    xdmf.write_mesh(mesh)
+    xdmf.write_meshtags(ft, mesh.geometry)
+
+
+model = F.HydrogenTransportProblem()
+model.volume_meshtags = ct
+model.facet_meshtags = ft
+
+salt = F.Material(D_0=100, E_D=0)
+
+top_surface = F.SurfaceSubdomain(id=1)
+volume = F.VolumeSubdomain(id=1, material=salt)
+model.subdomains = [top_surface, volume]
+
+model.mesh = F.Mesh(mesh)
+
+T = F.Species("T")
+model.species = [T]
+
+model.boundary_conditions = [
+    F.FixedConcentrationBC(subdomain=top_surface, value=0.0, species=T)
+]
+
+model.sources = [F.ParticleSource(value=1, volume=volume, species=T)]
+
+model.temperature = 650.0
+
+model.settings = F.Settings(atol=1e0, rtol=1e-10, transient=True, final_time=10)
+
+model.settings.stepsize = 1
+
+model.exports = [
+    F.VTXSpeciesExport(filename="tritium_conc.bp", field=T),
+]
+
+# from dolfinx import log
+
+# log.set_log_level(log.LogLevel.INFO)
+
+model.initialise()
+model.run()
